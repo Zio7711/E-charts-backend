3e1bfaf3ee391a087ede62fbfb77e0dbb7a94cf8
zio7711 <zi.ao.tan@outlook.com>
2022-01-22 18:23:37 +0800

    set output route

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const { resolve } = require('path');
 module.exports = {
   node: {
     net: 'empty',
     fs: 'empty',
+  },
+
+  output: {
+    filename: 'chart.js',
+    path: resolve(__dirname, 'build'),
   },
 };
