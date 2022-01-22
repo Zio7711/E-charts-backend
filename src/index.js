6f257bcf8c003291fcfa63fab2acb599f1f13a50
zio7711 <zi.ao.tan@outlook.com>
2022-01-22 17:14:37 +0800

    set up backend data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 服务器入口文件
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 1.创建koa实例对象
+const Koa = require('koa');
+const app = new Koa();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 2.绑定中间件
+
+// 绑定第一层中间件(计算总耗时中间件)
+const respDurationMiddleware = require('./middleware/koa_response_duration');
+app.use(respDurationMiddleware);
+
+// 绑定第二层中间件(响应头中间件)
+const respHeaderMiddleware = require('./middleware/koa_response_header');
+app.use(respHeaderMiddleware);
+
+// 绑定第三层中间件(读取文件数据)
+const respDataMiddleware = require('./middleware/koa_response_data');
+app.use(respDataMiddleware);
+
+// 3.监听端口
+app.listen(9997, () => {
+  // 只有请求地图数据时，用到了 9997端口
+  console.log(
+    'Server Start Complete: \thttp://localhost:9997 \t ws://localhost:9998'
+  );
+});
+
+const WebSocketService = require('./service/web_socket_service');
+// 开启服务端的监听，监听客户端的连接
+// 当某一个客户端连接成功之后，就会对这个客户端进行 message 事件的监听
+WebSocketService.listen();
